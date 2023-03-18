aecd98de3d1b1bdf515abbe18d3904bb277855a4
Andrew Stone <g.andrew.stone@gmail.com>
2023-03-18 18:36:05 -0400

    remove unused welcome screens, add some translations

--- app/src/main/java/info/bitcoinunlimited/www/wally/Welcome.kt
+++ app/src/main/java/info/bitcoinunlimited/www/wally/Welcome.kt
@@ -43,8 +43,6 @@ class Welcome : CommonNavActivity()
 
 
 /**
- *
- */
 class WelcomeFragment1 : Fragment()
 {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View
@@ -55,112 +53,10 @@ class WelcomeFragment1 : Fragment()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?)
     {
         super.onViewCreated(view, savedInstanceState)
+
         view.findViewById<Button>(R.id.button_first).setOnClickListener {
             findNavController().navigate(R.id.action_Welcome1_to_Welcome2)
         }
     }
 }
-
-class WelcomeFragment2 : Fragment()
-{
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View
-    {
-        return Welcomef2Binding.inflate(inflater).root
-    }
-
-    override fun onResume()
-    {
-        super.onResume()
-        if (wallyApp!!.accounts.isNotEmpty())
-        {
-            findNavController().navigate(R.id.action_NewAccount_to_Backup)
-        }
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
-    {
-        super.onViewCreated(view, savedInstanceState)
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            if (wallyApp!!.accounts.isEmpty())
-            {
-                val intent = Intent(appContext!!.context, NewAccount::class.java)
-                startActivity(intent)
-            }
-            else
-            {
-                findNavController().navigate(R.id.action_NewAccount_to_Backup)
-            }
-        }
-    }
-}
-
-class WelcomeFragment3 : Fragment()
-{
-    public lateinit var ui: Welcomef3Binding
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View
-    {
-        ui = Welcomef3Binding.inflate(inflater)
-        return ui.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
-    {
-        super.onViewCreated(view, savedInstanceState)
-
-        val accounts = wallyApp!!.accounts
-        var wordSeeds: String = ""
-        for (c in accounts)  // grab the 1st primary account
-        {
-            if (c.value.wallet.chainSelector == PRIMARY_CRYPTO)
-            {
-                wordSeeds = c.value.wallet.secretWords + "\n"
-                break
-            }
-        }
-        if (wordSeeds == "") wordSeeds = accounts.values.first().wallet.secretWords
-        ui.welcomeWalletBackupSecret.text = wordSeeds
-
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_Backup_to_Identity)
-        }
-    }
-}
-
-class WelcomeIdentity : Fragment()
-{
-    public lateinit var ui: Welcomef4Binding
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View
-    {
-        ui = Welcomef4Binding.inflate(inflater)
-        return ui.root
-    }
-
-    override fun onResume()
-    {
-        super.onResume()
-        val act = activity as Welcome
-        if (act.enteredIdentity)
-        {
-            ui.welcomeIdentityText.text = i18n(R.string.welcomeComplete)
-        }
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
-    {
-        val act = activity as Welcome
-        super.onViewCreated(view, savedInstanceState)
-
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            if (!act.enteredIdentity) // wallyApp!!.accounts.size==0)
-            {
-                val intent = Intent(appContext!!.context, IdentitySettings::class.java)
-                act.enteredIdentity = true
-                startActivity(intent)
-            }
-            else
-            {
-                act.finish()
-            }
-        }
-    }
-}
+*/
