f52b6d5a58c55065f3adf666fe81f17d0c0bbe11
Jørgen Svennevik Notland <jorgen.notland@protonmail.com>
2023-09-19 16:19:16 +0200

    Add actual + TODO statements for utils.macos.kt
    Commend isUiThread because it was causing a build crash and macOs uses jvm

--- src/src/macosMain/kotlin/utils_macos.kt
+++ src/src/macosMain/kotlin/utils_macos.kt
@@ -1,11 +1,34 @@
 package info.bitcoinunlimited.www.wally
 
-import platform.UIKit.UIApplication
+//import platform.UIKit.UIApplication
 
 /** Returns true if this function is called within the UI thread
  * Many platforms have specific restrictions on what can be run within the UI (often the "main") thread.
  */
 actual fun isUiThread(): Boolean
 {
-    return UIApplication.sharedApplication.isMainThread
+    TODO("Not yet implemented")
+    //return UIApplication.sharedApplication.isMainThread
+}
+
+/** Converts an encoded URL to a raw string */
+actual fun String.urlDecode(): String {
+    TODO("Not yet implemented")
+}
+
+/** Converts a string to a string that can be put into a URL */
+actual fun String.urlEncode(): String
+{
+    TODO("Not yet implemented")
+}
+
+/** Get the clipboard.  Platforms that have a clipboard history should return that history, with the primary clip in index 0 */
+actual fun getTextClipboard(): List<String>
+{
+    TODO("Not yet implemented")
+}
+
+/** Sets the clipboard, potentially asynchronously. */
+actual fun setTextClipboard(msg: String)
+{
 }
