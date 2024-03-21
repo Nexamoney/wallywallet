0a82da86a9e6d13b52dda0b9377ec599ed996f06
Andrew Stone <g.andrew.stone@gmail.com>
2024-03-21 17:32:43 -0400

    bump lnk version

--- src/build.gradle.kts
+++ src/build.gradle.kts
@@ -7,7 +7,7 @@ import java.net.URL
 // Dependency versions
 val mpThreadsVersion = "0.2.4"
 val nexaRpcVersion = "1.1.5"
-val libNexaKotlinVersion = "0.1.41"
+val libNexaKotlinVersion = "0.1.43"
 
 val serializationVersion = "1.6.3"  // https://github.com/Kotlin/kotlinx.serialization
 val coroutinesVersion = "1.8.0"     // https://github.com/Kotlin/kotlinx.coroutines
