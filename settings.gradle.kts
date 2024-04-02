210a2b9d4f850dd96297cf0a13d377baddf6231e
Andrew Stone <g.andrew.stone@gmail.com>
2024-04-02 15:13:20 -0400

    remove more android activities, fix bug in preferred string parsing, use new lnk with sync opts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,14 +9,15 @@ pluginManagement {
 
 dependencyResolutionManagement {
     repositories {
-        mavenLocal()
-        maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev/") }  // mpthreads
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+        maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev/") }
         maven { url = uri("https://gitlab.com/api/v4/projects/48544966/packages/maven") }  // mpthreads
         maven { url = uri("https://gitlab.com/api/v4/projects/38119368/packages/maven") }  // libnexarpc
         maven { url = uri("https://gitlab.com/api/v4/projects/48545045/packages/maven") }  // Libnexakotlin
         maven { url = uri("https://jitpack.io") }
-        google()
-        gradlePluginPortal()
+        mavenLocal()
     }
 }
 
