04c4dc5c0647e02ec30dac9f840ff5350c998579
Jørgen Svennevik Notland <jorgen.notland@protonmail.com>
2024-05-08 16:27:31 +0000

    Wally SelectionTextStyle fallback font size + Test

--- src/src/commonTest/kotlin/ui/theme/ThemeTest.kt
+++ src/src/commonTest/kotlin/ui/theme/ThemeTest.kt
@@ -5,14 +5,17 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.test.*
+import info.bitcoinunlimited.www.wally.ui.theme.FontScale
+import info.bitcoinunlimited.www.wally.ui.theme.FontScaleStyle
 import info.bitcoinunlimited.www.wally.ui.theme.WallyRoundedButton
+import info.bitcoinunlimited.www.wally.ui.theme.WallySectionTextStyle
 import kotlin.test.Test
 
+@OptIn(ExperimentalTestApi::class)
 class ThemeTest
 {
-
-    @OptIn(ExperimentalTestApi::class)
     @Test
     fun wallyRoundedButtonTest() = runComposeUiTest {
         setContent {
@@ -33,4 +36,38 @@ class ThemeTest
         onNodeWithText("Button").performClick()
         onNodeWithText("3").assertIsDisplayed()
     }
+
+    @Test
+    fun wallySelectionTextStyleTest() = runComposeUiTest {
+        setContent {
+            Text(
+              text = "text",
+              modifier = Modifier,
+              style = WallySectionTextStyle()
+            )
+        }
+    }
+
+    @Test
+    fun wallyFontScaleTest() = runComposeUiTest {
+        setContent {
+            Text(
+              text = "text",
+              modifier = Modifier,
+              fontSize = FontScale(0.75)
+            )
+        }
+    }
+
+    @Test
+    fun wallyFontFontScaleStyleTest() = runComposeUiTest {
+        setContent {
+            Text(
+              text = "text",
+              modifier = Modifier,
+              style = FontScaleStyle(2.0)
+            )
+        }
+    }
+
 }
