aa22f2bd401a9a042e6416d9469e87fe034305c6
Jørgen Svennevik Notland <jorgen.notland@protonmail.com>
2024-05-09 17:02:49 +0000

    Basic Compose UI Test for AccountDetailScreen

---
+++ src/src/commonTest/kotlin/ui/AccountDetailScreenTest.kt
@@ -0,0 +1,50 @@
+package ui
+
+import androidx.compose.ui.test.*
+import info.bitcoinunlimited.www.wally.*
+import info.bitcoinunlimited.www.wally.ui.AccountDetailScreen
+import info.bitcoinunlimited.www.wally.ui.ScreenNav
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.runBlocking
+import org.nexa.libnexakotlin.ChainSelector
+import org.nexa.libnexakotlin.initializeLibNexa
+import org.nexa.libnexakotlin.runningTheTests
+import kotlin.test.Test
+
+@OptIn(ExperimentalTestApi::class)
+class AccountDetailScreenTest
+{
+
+    init {
+        initializeLibNexa()
+        runningTheTests = true
+        forTestingDoNotAutoCreateWallets = true
+        dbPrefix = "test_"
+    }
+
+    @Test
+    fun accountDetailScreenTest() = runComposeUiTest {
+
+        val cs = ChainSelector.NEXA
+        lateinit var account: Account
+        wallyApp = CommonApp()
+        wallyApp!!.onCreate()
+        wallyApp!!.openAllAccounts()
+        runBlocking(Dispatchers.IO) {
+            account = wallyApp!!.newAccount("itemvie", 0U, "", cs)!!
+        }
+        setContent {
+            AccountDetailScreen(account, ScreenNav())
+        }
+
+        /**
+         * Open change pin View and click cancel button to close it.
+         */
+        onNodeWithText(i18n(S.AccountStatistics)).isDisplayed()
+        onNodeWithText(i18n(S.AccountActions)).isDisplayed()
+        onNodeWithText(i18n(S.SetChangePin)).performClick()
+        onNodeWithText(i18n(S.PinHidesAccount)).isDisplayed()
+        onNodeWithText(i18n(S.cancel)).performClick()
+    }
+}
