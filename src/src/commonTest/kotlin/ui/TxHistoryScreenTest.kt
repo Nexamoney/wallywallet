a3dc9e145640feb2e5ca12d984628ea2680dab58
Jørgen Svennevik Notland <jorgen.notland@protonmail.com>
2024-05-09 17:03:56 +0000

    Basic Compose UI test for TxHistoryScreen

---
+++ src/src/commonTest/kotlin/ui/TxHistoryScreenTest.kt
@@ -0,0 +1,40 @@
+package ui
+
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.runComposeUiTest
+import info.bitcoinunlimited.www.wally.*
+import info.bitcoinunlimited.www.wally.ui.ScreenNav
+import info.bitcoinunlimited.www.wally.ui.TxHistoryScreen
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.runBlocking
+import org.nexa.libnexakotlin.ChainSelector
+import org.nexa.libnexakotlin.initializeLibNexa
+import org.nexa.libnexakotlin.runningTheTests
+import kotlin.test.Test
+
+@OptIn(ExperimentalTestApi::class)
+class TxHistoryScreenTest
+{
+    init {
+        initializeLibNexa()
+        runningTheTests = true
+        forTestingDoNotAutoCreateWallets = true
+        dbPrefix = "test_"
+    }
+
+    @Test
+    fun txHistoryScreenTest() = runComposeUiTest {
+        val cs = ChainSelector.NEXA
+        lateinit var account: Account
+        wallyApp = CommonApp()
+        wallyApp!!.onCreate()
+        wallyApp!!.openAllAccounts()
+        runBlocking(Dispatchers.IO) {
+            account = wallyApp!!.newAccount("txhis", 0U, "", cs)!!
+        }
+        setContent {
+            TxHistoryScreen(account, ScreenNav())
+        }
+    }
+}
