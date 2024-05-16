8ba9304e28889994d4ea1a91d63be1c3a9b78091
Jørgen Svennevik Notland <jorgen.notland@protonmail.com>
2024-05-16 18:50:45 +0200

    Remove grep test example from UnitTests.kt

--- src/src/commonTest/kotlin/UnitTests.kt
+++ src/src/commonTest/kotlin/UnitTests.kt
@@ -10,36 +10,6 @@ import kotlin.test.*
 
 class CommonTest
 {
-    companion object
-    {
-        val sampleData = listOf(
-          "123 abc",
-          "abc 123",
-          "123 ABC",
-          "ABC 123"
-        )
-    }
-
-    fun grep(lines: List<String>, pattern: String, action: (String) -> Unit)
-    {
-        val regex = pattern.toRegex()
-        lines.filter(regex::containsMatchIn).forEach(action)
-    }
-
-    @Test
-    fun shouldFindMatches()
-    {
-        val results = mutableListOf<String>()
-        grep(sampleData, "[a-z]+") {
-            results.add(it)
-        }
-
-        assertEquals(2, results.size)
-        for (result in results) {
-            assertContains(result, "abc")
-        }
-    }
-
     @Test
     fun testManyNfts()
     {
