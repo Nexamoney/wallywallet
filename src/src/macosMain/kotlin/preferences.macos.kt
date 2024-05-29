f91af0fdab347cd354ae3f693bd8c57d7b911f73
Jørgen Svennevik Notland <jorgen.notland@protonmail.com>
2024-05-29 11:14:57 +0000

    Remove unused macOS (Native) target

--- src/src/macosMain/kotlin/preferences.macos.kt
+++
@@ -1,9 +0,0 @@
-package info.bitcoinunlimited.www.wally
-
-actual val PREF_MODE_PRIVATE: Int
-    get() = TODO("Not yet implemented")
-
-actual fun getSharedPreferences(prefDbName: String, mode: Int): SharedPreferences
-{
-    TODO("Not yet implemented")
-}
--- src/src/macosMain/kotlin/ui/views/LoadingAnimationView.macos.kt
+++
@@ -1,8 +0,0 @@
-package info.bitcoinunlimited.www.wally.ui.views
-
-import androidx.compose.runtime.Composable
-
-@Composable
-actual fun LoadingAnimationContent() {
-    // TODO: Implement
-}
