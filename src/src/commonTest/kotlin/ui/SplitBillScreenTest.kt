0cd8c42ebec9fc00bd001537a958c619a41cce13
Jørgen Svennevik Notland <jorgen.notland@protonmail.com>
2024-07-03 15:16:54 +0200

    SplitBillScreenTest: Enter amount and tip and verify on screen

---
+++ src/src/commonTest/kotlin/ui/SplitBillScreenTest.kt
@@ -0,0 +1,68 @@
+package ui
+
+import androidx.compose.ui.test.*
+import info.bitcoinunlimited.www.wally.*
+import info.bitcoinunlimited.www.wally.ui.ScreenNav
+import info.bitcoinunlimited.www.wally.ui.SplitBillScreen
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.runBlocking
+import org.nexa.libnexakotlin.ChainSelector
+import org.nexa.libnexakotlin.initializeLibNexa
+import org.nexa.libnexakotlin.runningTheTests
+import kotlin.test.Test
+
+class SplitBillScreenTest
+{
+    init {
+        initializeLibNexa()
+        runningTheTests = true
+        forTestingDoNotAutoCreateWallets = true
+        dbPrefix = "test_"
+    }
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun displaySplitBillScreen() = runComposeUiTest {
+        val cs = ChainSelector.NEXA
+        lateinit var account: Account
+        wallyApp = CommonApp()
+        wallyApp!!.onCreate()
+        wallyApp!!.openAllAccounts()
+        runBlocking(Dispatchers.IO) {
+            account = wallyApp!!.newAccount("mock", 0U, "", cs)!!
+        }
+        setContent {
+            SplitBillScreen(ScreenNav(), account)
+        }
+
+        onNodeWithText(i18n(S.SplitBillDescription)).assertIsDisplayed()
+    }
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun amountInputTest() = runComposeUiTest {
+        val cs = ChainSelector.NEXA
+        lateinit var account: Account
+        wallyApp = CommonApp()
+        wallyApp!!.onCreate()
+        wallyApp!!.openAllAccounts()
+        runBlocking(Dispatchers.IO) {
+            account = wallyApp!!.newAccount("mock", 0U, "", cs)!!
+        }
+        setContent {
+            SplitBillScreen(ScreenNav(), account)
+        }
+
+        onNodeWithTag("SplitBillScreenAmountInput").assertIsDisplayed()
+        onNodeWithTag("SplitBillScreenAmountInput").performTextClearance()
+        onNodeWithTag("SplitBillScreenAmountInput").performTextInput("100")
+        onNodeWithTag("SplitBillScreenAmountInput").assertTextEquals("100")
+
+        onNodeWithTag("SplitBillScreenTipInput").assertIsDisplayed()
+        onNodeWithTag("SplitBillScreenTipInput").performTextClearance()
+        onNodeWithTag("SplitBillScreenTipInput").performTextInput("42")
+
+        onNodeWithText("42").assertIsDisplayed()
+    }
+}
