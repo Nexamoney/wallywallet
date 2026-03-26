79db520235a81648b2df3f227c8801c00ad54d0d
Jørgen Svennevik Notland <jorgen.notland@protonmail.com>
2026-03-26 19:30:59 +0100

    Set and change account pin test

--- src/src/commonTest/kotlin/ui/AccountDetailScreenTest.kt
+++ src/src/commonTest/kotlin/ui/AccountDetailScreenTest.kt
@@ -1,10 +1,6 @@
 package ui
 
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.test.*
-import androidx.lifecycle.ViewModelStore
-import androidx.lifecycle.ViewModelStoreOwner
-import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import info.bitcoinunlimited.www.wally.*
 import info.bitcoinunlimited.www.wally.ui.AccountDetailScreen
 import info.bitcoinunlimited.www.wally.ui.AccountStatisticsViewModelFake
@@ -18,7 +14,7 @@ import kotlin.test.Test
 class AccountDetailScreenTest:WallyUiTestBase()
 {
     @Test
-    fun accountDetailScreenTest()
+    fun viewPinTest()
     {
         val cs = ChainSelector.NEXA
         val account = wallyApp!!.newAccount("sendScreenContentTest", 0U, "", cs)!!
@@ -48,6 +44,59 @@ class AccountDetailScreenTest:WallyUiTestBase()
             waitForCatching { onNodeWithText(i18n(S.PinHidesAccount)).isDisplayed() }
             onNodeWithText(i18n(S.cancel)).performClick()
             settle()
+        }
+        wallyApp!!.deleteAccount(account)
+    }
+
+    @Test
+    fun setAndChangePinTest()
+    {
+        val cs = ChainSelector.NEXA
+        val account = wallyApp!!.newAccount("sendScreenContentTest", 0U, "", cs)!!
+
+        runComposeUiTest {
+            /*
+                Set selected account to populate the UI
+            */
+            setSelectedAccount(account)
+
+            val ap = AccountPillViewModelFake(MutableStateFlow(account))
+            val accountStatsViewModel = AccountStatisticsViewModelFake(account)
+
+            setContent {
+                AccountDetailScreen(accountStatsViewModel, ap)
+            }
+            settle()
+
+            onNodeWithText(i18n(S.AutomaticNewAddress)).assertIsDisplayed()
+
+            /**
+             * Set pin to 777
+             */
+            waitForCatching { onNodeWithText(i18n(S.AccountStatistics)).isDisplayed() }
+            onNodeWithText(i18n(S.SetChangePin)).performClick()
+            settle()
+            onNodeWithText(i18n(S.PinHidesAccount)).isDisplayed()
+            onNodeWithText(i18n(S.EnterPINorBlankToRemove)).requestFocus()
+            onNodeWithText(i18n(S.EnterPINorBlankToRemove)).performTextInput("7777")
+            onNodeWithText(i18n(S.accept)).performClick()
+
+            /**
+             * Change pin to 8888
+             */
+            onNodeWithText(i18n(S.SetChangePin)).performClick()
+            onNodeWithText(i18n(S.CurrentPin)).assertIsDisplayed()
+            onNodeWithText(i18n(S.NewPin)).assertIsDisplayed()
+            onNodeWithTag("CurrentPinCheckIcon").assertDoesNotExist()
+            onNodeWithText(i18n(S.EnterPIN)).requestFocus()
+            onNodeWithText(i18n(S.EnterPIN)).performTextInput("7777")
+
+            onNodeWithText(i18n(S.EnterPINorBlankToRemove)).requestFocus()
+            onNodeWithText(i18n(S.EnterPINorBlankToRemove)).performTextInput("8888")
+            settle()
+            onNodeWithTag("CurrentPinCheckIcon").assertIsDisplayed()
+            onNodeWithText(i18n(S.accept)).performClick()
+            onNodeWithText(i18n(S.SetChangePin)).assertIsDisplayed()
         }
         wallyApp!!.deleteAccount(account)
     }
