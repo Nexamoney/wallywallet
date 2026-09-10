898d8ded0b9776ad9decccf78b597016c690f00f
Andrew Stone <g.andrew.stone@gmail.com>
2026-09-10 10:54:45 -0400

    ensure that a large rendered recovery secret fits

--- shared/src/commonTest/kotlin/ui/AccountDetailScreenTest.kt
+++ shared/src/commonTest/kotlin/ui/AccountDetailScreenTest.kt
@@ -1,15 +1,26 @@
 package ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.test.*
 import androidx.compose.ui.util.fastJoinToString
 import info.bitcoinunlimited.www.wally.*
 import info.bitcoinunlimited.www.wally.ui.AccountDetailScreen
 import info.bitcoinunlimited.www.wally.ui.AccountStatisticsViewModelFake
+import info.bitcoinunlimited.www.wally.ui.VisualOverflowKey
 import info.bitcoinunlimited.www.wally.ui.setSelectedAccount
 import info.bitcoinunlimited.www.wally.ui.views.AccountPillViewModelFake
 import kotlinx.coroutines.flow.MutableStateFlow
 import org.nexa.libnexakotlin.ChainSelector
+import org.nexa.libnexakotlin.UnsecuredSecret
+import org.nexa.libnexakotlin.decodeUtf8
+import org.nexa.libnexakotlin.encodeUtf8
+import org.nexa.libnexakotlin.englishWordList
+import org.nexa.libnexakotlin.generateBip39Seed
 import kotlin.test.Test
+import kotlin.test.assertTrue
 
 @OptIn(ExperimentalTestApi::class)
 class AccountDetailScreenTest:WallyUiTestBase()
@@ -157,6 +168,87 @@ class AccountDetailScreenTest:WallyUiTestBase()
                 onNodeWithText(i18n(S.ViewRecoveryPhrase)).assertIsDisplayed()
             }
 
+            }
+            finally
+            {
+                wallyApp!!.deleteAccount(account)
+            }
+        }
+    }
+
+    @Test
+    fun viewBiggestRecoveryPhrase()
+    {
+        var longestWord = "mushroom"
+        // We don't really need to do this every time since the longest word was calced as "mushroom".  But maybe font changes or we want to
+        // support non-english words lists in the future.
+        androidx.compose.ui.test.v2.runComposeUiTest {
+            val words = englishWordList
+
+            setContent {
+                Column {
+                    words.forEach { word ->
+                        BasicText(
+                          text = word,
+                          modifier = Modifier.testTag(word)
+                        )
+                    }
+                }
+            }
+
+            longestWord = words.maxBy { word ->
+                val ret = onNodeWithTag(word)
+                  .fetchSemanticsNode().size.width
+                // println("$word: $ret")
+                ret
+            }
+        }
+        println("Longest rendered word: $longestWord")
+
+        secureViewEnabled = false
+
+        listOf(ChainSelector.NEXATESTNET).forEach { cs ->
+            val account = mockAccount(chainSelector = cs)
+            account.wallet.secretWords = UnsecuredSecret(List(12) { longestWord }.joinToString(" ").encodeUtf8())
+            account.wallet.secret = UnsecuredSecret(generateBip39Seed(account.wallet.secretWords.getSecret().decodeUtf8(), ""))
+            try
+            {
+                val mnemonic = account.getRecoveryPhrase()
+                val mnemonicFormatted = mnemonic.split(" ").chunked(4).fastJoinToString("\n") { it.fastJoinToString(" ") }
+
+                androidx.compose.ui.test.v2.runComposeUiTest {
+                    // Set selected account to populate the UI
+                    setSelectedAccount(account)
+
+                    val ap = AccountPillViewModelFake(MutableStateFlow(account))
+                    val accountStatsViewModel = AccountStatisticsViewModelFake(account)
+
+                    setContent {
+                        AccountDetailScreen(accountStatsViewModel, ap)
+                    }
+                    settle()
+
+                    onNodeWithText(i18n(S.ViewRecoveryPhrase)).assertIsDisplayed()
+                    onNodeWithText(i18n(S.ViewRecoveryPhrase)).performClick()
+                    settle()
+                    onNodeWithText(i18n(S.recoveryPhrase)).assertIsDisplayed()
+
+                    val node = onNodeWithTag("recoveryPhraseWords", true)
+
+                    // Check that the box we display it in is not overflowing its parent
+                    val semnode = node.fetchSemanticsNode()
+                    val root = onRoot().fetchSemanticsNode()
+                    val bounds = semnode.boundsInRoot
+                    val rootBounds = root.boundsInRoot
+                    assertTrue(bounds.left >= rootBounds.left &&
+                      bounds.top >= rootBounds.top &&
+                      bounds.right <= rootBounds.right &&
+                      bounds.bottom <= rootBounds.bottom)
+
+                    // This is really the important one for text display.
+                    // Check that we didn't mark it as having visual overflow during text layout.
+                    node.assert(SemanticsMatcher.expectValue(VisualOverflowKey, false))
+                }
             }
             finally
             {
